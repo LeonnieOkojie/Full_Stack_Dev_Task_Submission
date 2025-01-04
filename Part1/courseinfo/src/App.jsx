5141524d5df7164f5362d282523cf94ed5eb7a0a
CodeWithWinnie <leonieokojie@gmail.com>
2025-01-04 07:13:33 +0100

    Task 1.1 - 1.5 Completed

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,37 +1,38 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p> {props.name} {props.exercises} </p>
+  )
+}
+
+const Content = (props) => {
+  const parts = props.course.parts;
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const parts = props.course.parts;
+  return (
+    <div>
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    </div>
+  )
+}
+
+
 const App = () => {
   
-  const Header = (props) => {
-    return (
-      <h1>{props.course.name}</h1>
-    )
-  }
-
-  const Part = (props) => {
-    return (
-      <p> {props.name} {props.exercises} </p>
-    )
-  }
-
-  const Content = (props) => {
-    const parts = props.course.parts;
-    return (
-      <div>
-        <Part name={parts[0].name} exercises={parts[0].exercises} />
-        <Part name={parts[1].name} exercises={parts[1].exercises} />
-        <Part name={parts[2].name} exercises={parts[2].exercises} />
-      </div>
-    )
-  }
-
-  const Total = (props) => {
-    const parts = props.course.parts;
-    return (
-      <div>
-        <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-      </div>
-    )
-  }
-
   const course = {
     name: 'Half Stack application development',
     parts: [
